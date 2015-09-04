68cbce73c1083951839e507d66f04baed6190103
SUPORTE03 <SUPORTE03@SUPORTE03>
2015-09-04 13:39:48 -0300

    Ajustado funções dos botões entrar e sair do sistema.

--- src/telas/washCar/LoginTela.java
+++ src/telas/washCar/LoginTela.java
@@ -1,18 +1,19 @@
 package telas.washCar;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.BevelBorder;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
 import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LoginTela extends JFrame {
 
@@ -20,6 +21,49 @@ public class LoginTela extends JFrame {
 	private JPanel jpnLogin;
 	private JTextField jtfLoginUsuario;
 	private JPasswordField jpfSenhaUsuario;
+	private JLabel jlbLogo;
+	private JLabel lblLoginDoUsurio;
+	private JLabel jlbSenhaUsuario;
+	private JButton jbtLogin;
+	private JButton jbtSair;	
+	
+	public void componentesTelaLogin() {
+		jlbLogo = new JLabel("");
+		jlbLogo.setIcon(new ImageIcon(LoginTela.class.getResource("/Imagens/washCar.jpeg")));
+		jlbLogo.setBounds(10, 11, 262, 206);
+		jpnLogin.add(jlbLogo);
+		
+		lblLoginDoUsurio = new JLabel("Login do Usu\u00E1rio");
+		lblLoginDoUsurio.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblLoginDoUsurio.setBounds(10, 228, 187, 14);
+		jpnLogin.add(lblLoginDoUsurio);
+		
+		jtfLoginUsuario = new JTextField();
+		jtfLoginUsuario.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		jtfLoginUsuario.setBounds(10, 253, 187, 20);
+		jpnLogin.add(jtfLoginUsuario);
+		jtfLoginUsuario.setColumns(10);
+		
+		jlbSenhaUsuario = new JLabel("Senha do Usu\u00E1rio");
+		jlbSenhaUsuario.setFont(new Font("Tahoma", Font.BOLD, 12));
+		jlbSenhaUsuario.setBounds(10, 284, 187, 14);
+		jpnLogin.add(jlbSenhaUsuario);
+		
+		jpfSenhaUsuario = new JPasswordField();
+		jpfSenhaUsuario.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		jpfSenhaUsuario.setBounds(10, 309, 187, 20);
+		jpnLogin.add(jpfSenhaUsuario);
+		
+		jbtLogin = new JButton("Entrar no Sistema");
+		jbtLogin.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		jbtLogin.setBounds(10, 340, 150, 23);
+		jpnLogin.add(jbtLogin);
+		
+		jbtSair = new JButton("Sair do Sistema");
+		jbtSair.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		jbtSair.setBounds(10, 374, 150, 23);
+		jpnLogin.add(jbtSair);
+	}
 
 	public static void main(String[] args) {
 		LoginTela login = new LoginTela();
@@ -37,40 +81,21 @@ public class LoginTela extends JFrame {
 		setLocationRelativeTo(null);
 		jpnLogin.setLayout(null);
 		
-		JLabel jlbLogo = new JLabel("");
-		jlbLogo.setIcon(new ImageIcon(LoginTela.class.getResource("/Imagens/washCar.jpeg")));
-		jlbLogo.setBounds(10, 11, 262, 206);
-		jpnLogin.add(jlbLogo);
+		componentesTelaLogin();
 		
-		JLabel lblLoginDoUsurio = new JLabel("Login do Usu\u00E1rio");
-		lblLoginDoUsurio.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblLoginDoUsurio.setBounds(10, 228, 187, 14);
-		jpnLogin.add(lblLoginDoUsurio);
+		jbtLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PrincipalTela principal = new PrincipalTela();
+				principal.setVisible(true);
+				dispose();
+			}
+		});
 		
-		jtfLoginUsuario = new JTextField();
-		jtfLoginUsuario.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		jtfLoginUsuario.setBounds(10, 253, 187, 20);
-		jpnLogin.add(jtfLoginUsuario);
-		jtfLoginUsuario.setColumns(10);
+		jbtSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 		
-		JLabel jlbSenhaUsuario = new JLabel("Senha do Usu\u00E1rio");
-		jlbSenhaUsuario.setFont(new Font("Tahoma", Font.BOLD, 12));
-		jlbSenhaUsuario.setBounds(10, 284, 187, 14);
-		jpnLogin.add(jlbSenhaUsuario);
-		
-		jpfSenhaUsuario = new JPasswordField();
-		jpfSenhaUsuario.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		jpfSenhaUsuario.setBounds(10, 309, 187, 20);
-		jpnLogin.add(jpfSenhaUsuario);
-		
-		JButton jbtLogin = new JButton("Entrar no Sistema");
-		jbtLogin.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		jbtLogin.setBounds(10, 340, 150, 23);
-		jpnLogin.add(jbtLogin);
-		
-		JButton jbtSair = new JButton("Sair do Sistema");
-		jbtSair.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		jbtSair.setBounds(10, 374, 150, 23);
-		jpnLogin.add(jbtSair);
 	}
 }
