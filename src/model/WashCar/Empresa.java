9f532b74f020359b73e1ba67f62227b18fc688a8
SUPORTE03 <SUPORTE03@SUPORTE03>
2015-09-04 14:23:30 -0300

    Criado classe empresa e seus atributos

---
+++ src/model/WashCar/Empresa.java
@@ -0,0 +1,126 @@
+package model.WashCar;
+
+public class Empresa {
+	
+	private Integer idEmpresa;
+	private String razaoSocial;
+	private String nomeFantasia;
+	private String cnpj;
+	private String inscricaoEstadual;
+	private String inscricaoMunicipal;
+	private String endereco;
+	private String bairro;
+	private Integer numero;
+	private String telefoneComercial;
+	private String telefoneCelular;
+	private String fax;
+	private String regimeTributario;
+	private Cidade cidade;
+	
+	public Empresa(Integer idEmpresa, String razaoSocial, String nomeFantasia,
+			String cnpj, String inscricaoEstadual, String inscricaoMunicipal,
+			String endereco, String bairro, Integer numero,
+			String telefoneComercial, String telefoneCelular, String fax,
+			String regimeTributario, Cidade cidade) {
+		super();
+		this.idEmpresa = idEmpresa;
+		this.razaoSocial = razaoSocial;
+		this.nomeFantasia = nomeFantasia;
+		this.cnpj = cnpj;
+		this.inscricaoEstadual = inscricaoEstadual;
+		this.inscricaoMunicipal = inscricaoMunicipal;
+		this.endereco = endereco;
+		this.bairro = bairro;
+		this.numero = numero;
+		this.telefoneComercial = telefoneComercial;
+		this.telefoneCelular = telefoneCelular;
+		this.fax = fax;
+		this.regimeTributario = regimeTributario;
+		this.cidade = cidade;
+	}
+	
+	public Integer getIdEmpresa() {
+		return idEmpresa;
+	}
+	public void setIdEmpresa(Integer idEmpresa) {
+		this.idEmpresa = idEmpresa;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getInscricaoEstadual() {
+		return inscricaoEstadual;
+	}
+	public void setInscricaoEstadual(String inscricaoEstadual) {
+		this.inscricaoEstadual = inscricaoEstadual;
+	}
+	public String getInscricaoMunicipal() {
+		return inscricaoMunicipal;
+	}
+	public void setInscricaoMunicipal(String inscricaoMunicipal) {
+		this.inscricaoMunicipal = inscricaoMunicipal;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getTelefoneComercial() {
+		return telefoneComercial;
+	}
+	public void setTelefoneComercial(String telefoneComercial) {
+		this.telefoneComercial = telefoneComercial;
+	}
+	public String getTelefoneCelular() {
+		return telefoneCelular;
+	}
+	public void setTelefoneCelular(String telefoneCelular) {
+		this.telefoneCelular = telefoneCelular;
+	}
+	public String getFax() {
+		return fax;
+	}
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	public String getRegimeTributario() {
+		return regimeTributario;
+	}
+	public void setRegimeTributario(String regimeTributario) {
+		this.regimeTributario = regimeTributario;
+	}
+	public Cidade getCidade() {
+		return cidade;
+	}
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}	
+}
