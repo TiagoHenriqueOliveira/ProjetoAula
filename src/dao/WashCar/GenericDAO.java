f1e7f50402101f8823a40c1397e3fecc7dc132e6
Tiago <Tiago@Tiago-PC>
2015-10-04 00:15:34 -0300

    Adicionado throws Exception

--- src/dao/WashCar/GenericDAO.java
+++ src/dao/WashCar/GenericDAO.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public interface GenericDAO<T> {
 
-	void inserir(T entidade);
+	void inserir(T entidade) throws Exception;
 	
-	void alterar(T entidade);
+	void alterar(T entidade) throws Exception;
 	
-	void excluir(T entidade);
+	void excluir(T entidade) throws Exception;
 	
 	T buscarId(Integer id);
 	
