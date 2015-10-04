fefc375b57aed8a9a376a980b076ec1b48337b53
Tiago <Tiago@Tiago-PC>
2015-10-04 16:32:45 -0300

    Criado regra para retornar o ultimo id cadastrado no banco.

--- src/dao/WashCar/MarcaDAO.java
+++ src/dao/WashCar/MarcaDAO.java
@@ -1,7 +1,12 @@
 package dao.WashCar;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import model.WashCar.Marca;
 
 public interface MarcaDAO extends GenericDAO<Marca>{
+	
+	public Integer obterUltimoID(PreparedStatement pstmt, ResultSet rs) throws Exception;
 	
 }
