264a3c2ba7dad5d2273cd9b5aa98bee8dda1dbdf
Tiago <Tiago@Tiago-PC>
2015-10-04 00:12:38 -0300

    Adicionado recurso para obter o ultimo id cadastrado no banco

--- src/dao/WashCar/UsuarioDAO.java
+++ src/dao/WashCar/UsuarioDAO.java
@@ -1,9 +1,14 @@
 package dao.WashCar;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import model.WashCar.Usuario;
 
 public interface UsuarioDAO extends GenericDAO<Usuario>{
 	
 	public Usuario login(String login, String senha);
+	
+	public Integer obterUltimoID(PreparedStatement pstmt, ResultSet rs) throws Exception;
 	
 }
