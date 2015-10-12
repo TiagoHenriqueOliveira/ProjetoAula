ed1f25538ab8e3c6708e5d1332d44086a8e7f06c
Tiago <Tiago@Tiago-PC>
2015-10-12 20:01:47 -0300

    Adicionado mais caractetes para validação.

--- src/validacaoCampos/WashCar/ValidaCampoString.java
+++ src/validacaoCampos/WashCar/ValidaCampoString.java
@@ -11,15 +11,11 @@ public class ValidaCampoString extends PlainDocument {
 
 	private static final long serialVersionUID = 1L;
 
-	public void insertString(int offset, String string,
-			javax.swing.text.AttributeSet setAtributo)
-			throws BadLocationException {
-			super.insertString(offset, string.toUpperCase().replaceAll("[^a-z|^A-Z|^ ]", ""), setAtributo);
+	public void insertString(int offset, String string, javax.swing.text.AttributeSet setAtributo) throws BadLocationException {
+			super.insertString(offset, string.toUpperCase().replaceAll("[^a-z|^A-Z|^/|^&|^ ]", ""), setAtributo);
 	}
 	
-	public void replace(int offset, String string,
-			javax.swing.text.AttributeSet setAtributo)
-			throws BadLocationException {
-			super.insertString(offset, string.toUpperCase().replaceAll("[^a-z|^A-Z|^ ]", ""), setAtributo);
+	public void replace(int offset, String string, javax.swing.text.AttributeSet setAtributo) 	throws BadLocationException {
+			super.insertString(offset, string.toUpperCase().replaceAll("[^a-z|^A-Z|^/|^&|^ ]", ""), setAtributo);
 	}
 }
