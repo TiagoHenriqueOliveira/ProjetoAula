6de72e436b670fd4d483335cc6838eee22e4dd3b
Tiago <Tiago@Tiago-PC>
2015-10-25 15:53:36 -0200

    Adicionado validação para campos alfa numéricos.

---
+++ src/validacaoCampos/WashCar/ValidaCampoAlfaNumerico.java
@@ -0,0 +1,21 @@
+package validacaoCampos.WashCar;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+public class ValidaCampoAlfaNumerico extends PlainDocument {
+	
+	/*
+	 * Objetivo dessa classe é para validar campos que irão receber somente letras
+	 */
+
+	private static final long serialVersionUID = 1L;
+
+	public void insertString(int offset, String string, javax.swing.text.AttributeSet setAtributo) throws BadLocationException {
+			super.insertString(offset, string.toUpperCase().replaceAll("[^a-z|^A-Z|^/|^&|^0-9|^ ]", ""), setAtributo);
+	}
+	
+	public void replace(int offset, String string, javax.swing.text.AttributeSet setAtributo) 	throws BadLocationException {
+			super.insertString(offset, string.toUpperCase().replaceAll("[^a-z|^A-Z|^/|^&|^0-9|^ ]", ""), setAtributo);
+	}
+}
