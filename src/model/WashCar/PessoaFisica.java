0919911942117c8be0a6ff3231ce4347afc627bd
Tiago <Tiago@Tiago-PC>
2015-11-02 15:37:18 -0200

    Adicionado classe Pessoa Fisica e Pessoa Juridica.

---
+++ src/model/WashCar/PessoaFisica.java
@@ -0,0 +1,46 @@
+package model.WashCar;
+
+import java.time.LocalDate;
+
+public class PessoaFisica {
+	
+	private String nome;
+	private String cpf;
+	private String rg;
+	private LocalDate dataNascimento;
+	
+	public PessoaFisica(String nome, String cpf, String rg, LocalDate dataNascimento) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.dataNascimento = dataNascimento;
+	}
+	public PessoaFisica() {
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+}
---
+++ src/model/WashCar/PessoaJuridica.java
@@ -0,0 +1,53 @@
+package model.WashCar;
+
+public class PessoaJuridica {
+	
+	private String razaoSocial;
+	private String nomeFantasia;
+	private String cnpj;
+	private String inscricaoEstadual;
+	private String inscricaoMunicipal;
+	
+	public PessoaJuridica(String razaoSocial, String nomeFantasia, String cnpj, String inscricaoEstadual, String inscricaoMunicipal) {
+		this.razaoSocial = razaoSocial;
+		this.nomeFantasia = nomeFantasia;
+		this.cnpj = cnpj;
+		this.inscricaoEstadual = inscricaoEstadual;
+		this.inscricaoMunicipal = inscricaoMunicipal;
+	}
+
+	public PessoaJuridica() {
+		
+	}
+	
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getInscricaoEstadual() {
+		return inscricaoEstadual;
+	}
+	public void setInscricaoEstadual(String inscricaoEstadual) {
+		this.inscricaoEstadual = inscricaoEstadual;
+	}
+	public String getInscricaoMunicipal() {
+		return inscricaoMunicipal;
+	}
+	public void setInscricaoMunicipal(String inscricaoMunicipal) {
+		this.inscricaoMunicipal = inscricaoMunicipal;
+	}	
+}
