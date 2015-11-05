5f9100b7cb68d7d3b8621d072cb04b5b891965ec
Tiago <Tiago@Tiago-PC>
2015-11-05 20:00:01 -0200

    Adicionado atributos.

--- src/dao/WashCar/PessoaFisicaDAO.java
+++ src/dao/WashCar/PessoaFisicaDAO.java
@@ -5,9 +5,9 @@ import model.WashCar.PessoaFisica;
 
 public interface PessoaFisicaDAO {
 	
-	public PessoaFisica inserir() throws RegistroExistente;
+	public PessoaFisica inserir(PessoaFisica pessoa) throws RegistroExistente;
 	
-	public PessoaFisica alterar() throws RegistroExistente;
+	public PessoaFisica alterar(PessoaFisica pessoa) throws RegistroExistente;
 	
 	public PessoaFisica buscarNome(String nome) throws RegistroExistente;
 	
