d1463d98e17ffa8196ae62167faba83f8524b7a8
Tiago <Tiago@Tiago-PC>
2015-11-05 20:28:38 -0200

    Ajustado metodos.

--- src/dao/WashCar/PessoaFisicaDAO.java
+++ src/dao/WashCar/PessoaFisicaDAO.java
@@ -1,16 +1,18 @@
 package dao.WashCar;
 
+import java.util.List;
+
 import exception.WashCar.RegistroExistente;
 import model.WashCar.PessoaFisica;
 
 public interface PessoaFisicaDAO {
 	
-	public PessoaFisica inserir(PessoaFisica pessoa) throws RegistroExistente;
+	void inserir(PessoaFisica pessoa) throws RegistroExistente;
 	
-	public PessoaFisica alterar(PessoaFisica pessoa) throws RegistroExistente;
+	void alterar(PessoaFisica pessoa) throws RegistroExistente;
 	
-	public PessoaFisica buscarNome(String nome) throws RegistroExistente;
+	List<PessoaFisica> buscarNome(String nome);
 	
-	public PessoaFisica buscarDocumento(String documento) throws RegistroExistente;
+	public PessoaFisica buscarDocumento(String documento);
 	
 }
