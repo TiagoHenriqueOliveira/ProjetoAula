b14983f648362f026799d765f3e0f902063899eb
Tiago <Tiago@Tiago-PC>
2015-11-11 00:42:06 -0200

    Adicionado metodo para verificar se a janela já está aberta. Adicionado opção para ver se o usuário realmente quer  sair do sistema ou não.

--- src/forms/WashCar/PrincipalForm.java
+++ src/forms/WashCar/PrincipalForm.java
@@ -5,6 +5,7 @@ import javax.swing.JPanel;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -29,15 +30,16 @@ public class PrincipalForm extends JFrame {
 	private JMenuItem jmiMarca;
 	private JMenuItem jmiModelo;
 	private JMenuItem jmiTipoServico;
-	private static UsuarioForm usuarioForm;
-	private static CarroForm carroForm;
-	private static ClienteForm clienteForm;
-	private static AgendaForm agendaForm;
-	private static OrdemServicoForm ordemServicoForm;
-	private static EmpresaForm empresaForm;
-	private static ModeloForm modeloForm;
-	private static MarcaForm marcaForm;
-	private static TipoServicoForm tipoServicoForm;
+	private static PrincipalForm principalForm = new PrincipalForm();
+	private static UsuarioForm usuarioForm = new UsuarioForm();
+	private static CarroForm carroForm = new CarroForm();
+	private static ClienteForm clienteForm = new ClienteForm();
+	private static AgendaForm agendaForm = new AgendaForm();
+	private static OrdemServicoForm ordemServicoForm = new OrdemServicoForm();
+	private static EmpresaForm empresaForm = new EmpresaForm();
+	private static ModeloForm modeloForm = new ModeloForm();
+	private static MarcaForm marcaForm = new MarcaForm();
+	private static TipoServicoForm tipoServicoForm = new TipoServicoForm();
 	
 	public void componentesTelaPrincipal() {
 		jmbPrincipal = new JMenuBar();
@@ -88,16 +90,26 @@ public class PrincipalForm extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == jmiAgenda) {
-					agendaForm = new AgendaForm();
-					agendaForm.setVisible(true);
+					if(agendaForm.isVisible()) {
+						JOptionPane.showMessageDialog(agendaForm, "Agenda de serviços, já está aberta!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+						agendaForm.requestFocus();
+					} else {
+						agendaForm = new AgendaForm();
+						agendaForm.setVisible(true);
+					}
 				}
 			}
 		});
 		jmiCarro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == jmiCarro) {
-					carroForm = new CarroForm();
-					carroForm.setVisible(true);
+					if(carroForm.isVisible()) {
+						JOptionPane.showMessageDialog(carroForm, "Cadastro de carro, já está aberto!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+						carroForm.requestFocus();
+					} else {
+						carroForm = new CarroForm();
+						carroForm.setVisible(true);
+					}
 				}
 			}
 		});
@@ -105,8 +117,13 @@ public class PrincipalForm extends JFrame {
 		jmiCliente.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == jmiCliente) {
-					clienteForm = new ClienteForm();
-					clienteForm.setVisible(true);
+					if(clienteForm.isVisible()) {
+						JOptionPane.showMessageDialog(clienteForm, "Cadastro de cliente, já está aberto!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+						clienteForm.requestFocus();
+					} else {
+						clienteForm = new ClienteForm();
+						clienteForm.setVisible(true);
+					}
 				}
 			}
 		});
@@ -114,16 +131,13 @@ public class PrincipalForm extends JFrame {
 		jmiCadastroUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == jmiCadastroUsuario) {
-					usuarioForm = new UsuarioForm();
-					usuarioForm.setVisible(true);
-				}
-			}
-		});
-		
-		jmiSair.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == jmiSair) {
-					System.exit(0);
+					if(usuarioForm.isVisible()) {
+						JOptionPane.showMessageDialog(usuarioForm, "Cadastro de usuário, já está aberto!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+						usuarioForm.requestFocus();
+					} else {
+						usuarioForm = new UsuarioForm();
+						usuarioForm.setVisible(true);
+					}
 				}
 			}
 		});
@@ -131,8 +145,13 @@ public class PrincipalForm extends JFrame {
 		jmiOrdemServico.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == jmiOrdemServico) {
-					ordemServicoForm = new OrdemServicoForm();
-					ordemServicoForm.setVisible(true);
+					if(ordemServicoForm.isVisible()) {
+						JOptionPane.showMessageDialog(ordemServicoForm, "Cadastro de Ordem de Serviço, já está aberta!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+						ordemServicoForm.requestFocus();
+					} else {
+						ordemServicoForm = new OrdemServicoForm();
+						ordemServicoForm.setVisible(true);
+					}
 				}
 			}
 		});
@@ -140,8 +159,13 @@ public class PrincipalForm extends JFrame {
 		jmiEmpresa.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == jmiEmpresa) {
-					empresaForm = new EmpresaForm();
-					empresaForm.setVisible(true);
+					if(empresaForm.isVisible()) {
+						JOptionPane.showMessageDialog(empresaForm, "Cadastro de empresa, já está aberto!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+						empresaForm.requestFocus();
+					} else {
+						empresaForm = new EmpresaForm();
+						empresaForm.setVisible(true);
+					}
 				}
 			}
 		});
@@ -149,8 +173,13 @@ public class PrincipalForm extends JFrame {
 		jmiMarca.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == jmiMarca) {
-					marcaForm = new MarcaForm();
-					marcaForm.setVisible(true);
+					if(marcaForm.isVisible()) {
+						JOptionPane.showMessageDialog(marcaForm, "Cadastro de marca, já está aberto!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+						marcaForm.requestFocus();
+					} else {
+						marcaForm = new MarcaForm();
+						marcaForm.setVisible(true);
+					}
 				}
 			}
 		});
@@ -158,8 +187,13 @@ public class PrincipalForm extends JFrame {
 		jmiModelo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == jmiModelo) {
-					modeloForm = new ModeloForm();
-					modeloForm.setVisible(true);
+					if(modeloForm.isVisible()) {
+						JOptionPane.showMessageDialog(modeloForm, "Cadastro de modelo, já está aberto!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+						modeloForm.requestFocus();
+					} else {
+						modeloForm = new ModeloForm();
+						modeloForm.setVisible(true);
+					}
 				}
 			}
 		});
@@ -167,8 +201,24 @@ public class PrincipalForm extends JFrame {
 		jmiTipoServico.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == jmiTipoServico) {
-					tipoServicoForm = new TipoServicoForm();
-					tipoServicoForm.setVisible(true);
+					if(tipoServicoForm.isVisible()) {
+						JOptionPane.showMessageDialog(usuarioForm, "Cadastro de tipo de serviço, já está aberto!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+						tipoServicoForm.requestFocus();
+					} else {
+						tipoServicoForm = new TipoServicoForm();
+						tipoServicoForm.setVisible(true);
+					}
+				}
+			}
+		});
+		
+		jmiSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource() == jmiSair) {
+					Integer valor = JOptionPane.showConfirmDialog(principalForm, "Deseja sair do sistema", "Confirmação", JOptionPane.YES_NO_OPTION);
+					if(valor == 0) {
+						System.exit(0);
+					}
 				}
 			}
 		});
