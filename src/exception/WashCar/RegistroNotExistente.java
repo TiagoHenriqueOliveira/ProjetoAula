8a9262e3f7deb9270fb18d8dde8766b4e1e3d7bc
Tiago <Tiago@Tiago-PC>
2015-11-12 20:08:04 -0200

    Ajustado excessão. Adicionado modelo ER correto. SQL Tabelas + Inserts

--- src/exception/WashCar/RegistroNotExistente.java
+++ src/exception/WashCar/RegistroNotExistente.java
@@ -1,21 +1,10 @@
 package exception.WashCar;
 
-public class RegistroNotExistente extends NullPointerException {
+public class RegistroNotExistente extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 	
 	public RegistroNotExistente(String message) {
 		super(message);
-	}
-	
-	public String usuarioNotExistente() {
-		String mensagem = "";
-		if(getMessage() != null) {
-			if(getMessage().contains("Exception in thread")) {
-				mensagem = "Nenhum usuário foi localizado.\n"
-						+ "Por gentileza, efetue uma nova pesquisa.";
-			}
-		}
-		return mensagem;
 	}
 }
