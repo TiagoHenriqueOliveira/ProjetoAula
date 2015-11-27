2d504397447b603c4c629fc795cde3f49c6887f1
Tiago <Tiago@Tiago-PC>
2015-11-26 23:24:38 -0200

    Removido import

--- src/forms/WashCar/ListaClienteForm.java
+++ src/forms/WashCar/ListaClienteForm.java
@@ -21,7 +21,6 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Window.Type;
 
 public class ListaClienteForm extends JFrame {
 
