eb1a3dac57c54d4cbf2f96590776318f73f442e1
Tiago <Tiago@Tiago-PC>
2015-11-29 21:45:30 -0200

    Trocado metodos de geração do relatório.

--- src/relatorio/RelatorioUtil.java
+++ src/relatorio/RelatorioUtil.java
@@ -1,34 +1,20 @@
 package relatorio;
 
 import java.sql.Connection;
-import java.util.List;
 import java.util.Map;
 
-import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.xml.JRXmlLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
 public class RelatorioUtil {
 
-	public void compileViewReport(String pathRelatorio, List<?> dados,
+	public void viewReport(String pathRelatorio, Connection con,
 			Map<String, Object> parametros) {
 		try {
-			JasperDesign jasperDesign = JRXmlLoader.load(pathRelatorio);
-			
-			JRDataSource dataSource = new JRBeanCollectionDataSource(dados, false);
-			
-			JasperReport jr = JasperCompileManager.compileReport(jasperDesign); 
-
-			JasperPrint jasperPrint = JasperFillManager.fillReport(jr, parametros, dataSource);
-			
+			JasperPrint jasperPrint = JasperFillManager.fillReport(pathRelatorio, parametros, con);
 			JasperViewer.viewReport(jasperPrint);
 		} catch (JRException e) {
 			e.printStackTrace();
